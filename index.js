346dffa4ab679bcebd63c3c0f72306ee09b56a16
Akshit Madan <akmadan1717@gmail.com>
2025-10-31 14:19:02 +0530

    init node project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Harness Deployed API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
